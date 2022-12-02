423186fa8f166dc1c6b244c7eba6872ec9856176
SurfyCrescent97 <107763166+SurfyCrescent97@users.noreply.github.com>
2022-12-02 20:36:30 +0000

    Update device.js

--- device.js
+++ device.js
@@ -1,5 +1,6 @@
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
 window.location.replace("https://surfycrescent97.github.io/AboutMe/indexmobile.html");
 }
-else
+else{
 window.location.replace("https://surfycrescent97.github.io/AboutMe/");  
+}
